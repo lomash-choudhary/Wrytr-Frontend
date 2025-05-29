b79cecba9085fefda5ceb1e9d5d11fe7da32702d
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-05-30 02:23:57 +0530

    added the navigation urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
-import NavBar from './components/NavBar.component'
+import "./App.css";
+import NavBar from "./components/NavBar.component";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <NavBar />
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route path="signup" element={<h1>signup page</h1>} />
+          <Route path="signin" element={<h1>sign in page</h1>} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
